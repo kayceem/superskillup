918bf85d2563327a4b6875a10643f47717a6d06a
Naresh Tamang <naresh@ramailo.tech>
2024-03-15 10:42:22 +0545

    changed url name to user_login and user_register

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app.user import views as user_views
 
 urlpatterns = [
     path("v1/admin_login/", admin_views.login_admin, name = 'admin-login'),
-    path("v1/login/", user_views.login_user, name= 'login-user'),
-    path("v1/register/", user_views.register_user, name = 'register-user'),
+    path("v1/user_login/", user_views.login_user, name= 'login-user'),
+    path("v1/user_register/", user_views.register_user, name = 'register-user'),
     path("v1/verify_otp/", user_views.check_otp, name = 'check-otp')
 ]
