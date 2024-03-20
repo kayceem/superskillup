8408ba0379304b7a5c31672c87f53ef69af0ae62
Mohan <mohan@ramailo.tech>
2024-03-20 18:36:09 +0545

    add:validation in questions to add topics and courses

--- app/question/serializer.py
+++ app/question/serializer.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from app.models import Question
 
+
 class QuestionSerilizer(serializers.ModelSerializer):
 
     class Meta:
         model = Question
         fields = ["id", "question", "helping_text", "level", "course", "topic", "sub_topic", "url"]
-        
+
+    def validate(self, attrs):
+        if attrs.get('sub_topic'):
+            attrs['topic'] = attrs.get('sub_topic').topic
+            attrs['course'] = attrs.get('topic').course
+        if attrs.get('topic'):
+            attrs['course'] = attrs.get('topic').course
+        return super().validate(attrs)
