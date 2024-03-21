23b9ea7117a464900c52b2cb24e05bf1e8421b4f
Naresh Tamang <naresh@ramailo.tech>
2024-03-21 16:00:23 +0545

    formated in structure

--- app/user_answer/views.py
+++ app/user_answer/views.py
@@ -6,6 +6,7 @@ from app.user.user import User
 from app.user_answer.serializer import UserAnswerSerializer
 from app.api import api
 
+
 @api_view(["GET"])
 @authentication_classes([UserAuthentication])
 def get_answer_by_user_id(request):
@@ -13,9 +14,10 @@ def get_answer_by_user_id(request):
     user = request.user
     data = UserAnswer.get_answer_by_user(user.id)
     if data:
-        serializer = UserAnswerSerializer(data, many = True)
+        serializer = UserAnswerSerializer(data, many=True)
         return response_builder.get_200_success_response("Data Fetched", serializer.data)
     return response_builder.get_404_not_found_response(api.USER_ANSWER_NOT_FOUND)
+
 
 @api_view(["POST"])
 @authentication_classes([UserAuthentication])
@@ -27,11 +29,12 @@ def add_answer(request):
     answer_obj = UserAnswer.get_answer_by_question(question)
     if answer_obj:
         return response_builder.get_400_bad_request_response(api.INVALID_INPUT, "Answer already submitted")
-    serializer = UserAnswerSerializer(data = data)
+    serializer = UserAnswerSerializer(data=data)
     if not serializer.is_valid():
         return response_builder.get_400_bad_request_response(api.INVALID_INPUT, serializer.errors)
     serializer.save()
     return response_builder.get_200_success_response("Answer successfully added", serializer.data)
+
 
 @api_view(["PUT", "PATCH"])
 @authentication_classes([UserAuthentication])
@@ -41,13 +44,14 @@ def update_answer(request, id):
     answer_obj = UserAnswer.get_answer_by_id(id)
     if answer_obj:
         if answer_obj.user == user:
-            serializer = UserAnswerSerializer(answer_obj,data = request.data,partial = True)
+            serializer = UserAnswerSerializer(answer_obj, data=request.data, partial=True)
             if serializer.is_valid():
                 serializer.save()
                 return response_builder.get_200_success_response("Answer updated", serializer.data)
             return response_builder.get_400_bad_request_response(api.INVALID_INPUT, serializer.errors)
         return response_builder.get_400_bad_request_response(api.UNAUTHORIZED, "Access denied")
     return response_builder.get_404_not_found_response(api.USER_ANSWER_NOT_FOUND)
+
 
 @api_view(["GET"])
 @authentication_classes([AdminAuthentication])
@@ -58,8 +62,3 @@ def get_answer_by_id(request, id):
         serializer = UserAnswerSerializer(data)
         return response_builder.get_200_success_response("Data fetched", serializer.data)
     return response_builder.get_404_not_found_response(api.USER_ANSWER_NOT_FOUND)
-
-
-
-
-
