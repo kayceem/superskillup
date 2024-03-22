25106351085642530448ff3bff431f7667f8d960
Naresh Tamang <naresh@ramailo.tech>
2024-03-22 16:34:30 +0545

    add: bussiness logic and accessor logic added

--- app/user_answer/accessor.py
+++ app/user_answer/accessor.py
@@ -4,13 +4,13 @@ from app.models import UserAnswer
 class UserAnswerAccessor:
 
     @staticmethod
-    def get_answer_by_user(user_id):
-        return UserAnswer.objects.filter(user=user_id).all()
+    def get_all_answer():
+        return UserAnswer.objects.all()
 
     @staticmethod
     def get_answer_by_id(id):
         return UserAnswer.objects.filter(id=id).first()
 
     @staticmethod
-    def get_answer_by_question(question):
-        return UserAnswer.objects.filter(question=question).first()
+    def get_answers_by_assignment(assign_id):
+        return UserAnswer.objects.filter(user_course_assignment=assign_id).all()
