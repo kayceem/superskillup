124e5741e565dad787c49e461a5c71b7e2449129
Mohan <mohan@ramailo.tech>
2024-04-05 19:06:42 +0545

    remove:clean code

--- app/gpt_review/admin_views.py
+++ app/gpt_review/admin_views.py
@@ -6,17 +6,6 @@ from app.gpt_review.serializer import GptReviewSerializer
 from app.api import api
 
 
-# @api_view(["GET"])
-# @authentication_classes([AdminAuthentication])
-# def get_gpt_review_by_id(request, id):
-#     response_builder = ResponseBuilder()
-#     gpt_review = GptReview.get_gpt_review_by_id(id)
-#     if not gpt_review:
-#         return response_builder.get_404_not_found_response(api.GPT_REVIEW_NOT_FOUND)
-#     serializer = GptReviewSerializer(gpt_review)
-#     return response_builder.get_200_success_response("Data Fetched", serializer.data)
-
-
 @api_view(["GET"])
 @authentication_classes([AdminAuthentication])
 def get_gpt_review_by_answer(request, answer_id):
