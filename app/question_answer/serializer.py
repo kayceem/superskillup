62981d4f02dc04308ebb22064bf60880249f4e05
Mohan <mohan@ramailo.tech>
2024-04-05 15:36:41 +0545

    fix:serializer

--- app/question_answer/serializer.py
+++ app/question_answer/serializer.py
@@ -1,4 +1,4 @@
-from django.forms import ValidationError
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from app.models import QuestionAnswer
 
@@ -8,8 +8,6 @@ class QuestionAnswerSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         question = attrs.get('question')
         user_course_enrollment = attrs.get('user_course_enrollment')
-        print(question.course)
-        print(user_course_enrollment.course)
         if question.course != user_course_enrollment.course:
             raise ValidationError("Question course must match with user's enrolled course.")
         return super().validate(attrs)
