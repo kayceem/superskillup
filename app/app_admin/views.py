db46ec567f6e03869398f215b924da23fd352254
Mohan <mohan@ramailo.tech>
2024-04-05 11:06:26 +0545

    add:admin login serializer

--- app/app_admin/views.py
+++ app/app_admin/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from app.app_admin.serializers import AdminLoginSerializer
 from app.utils import utils
 from app.api.response_builder import ResponseBuilder
 from app.app_admin.app_admin import Admin
@@ -12,11 +13,10 @@ def login_admin(request):
     """
 
     response_builder = ResponseBuilder()
-    username = request.data.get("username")
-    password = request.data.get("password")
-    if not (username and password):
-        return response_builder.get_400_bad_request_response(api.INVALID_INPUT, {})
-    status, auth_token = Admin.login(username, password)
+    serializer = AdminLoginSerializer(data=request.data)
+    if not serializer.is_valid():
+        return response_builder.get_400_bad_request_response(api.INVALID_INPUT, serializer.errors)
+    status, auth_token = Admin.login(serializer.validated_data['username'], serializer.validated_data['password'])
     if utils.is_status_failed(status):
         return response_builder.get_200_fail_response(status)
 
