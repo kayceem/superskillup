dfb41945e8e3161041f9816960c729d35328776b
Mohan <mohan@ramailo.tech>
2024-04-22 11:08:35 +0545

    update:login

--- app/app_admin/serializers.py
+++ app/app_admin/serializers.py
@@ -13,8 +13,3 @@ class AdminSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username']
         extra_kwargs = {'id': {'read_only': True}, 'username': {'read_only': True}}
-
-
-class AdminLoginSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=255)
-    password = serializers.CharField(max_length=255)
