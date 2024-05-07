b31bcad9c84f13f32628c1a913436bac3caddeb9
Mohan <mohan@ramailo.tech>
2024-05-07 14:46:15 +0545

    update:count to annonate

--- app/user_video_watched/accessor.py
+++ app/user_video_watched/accessor.py
@@ -1,15 +1,16 @@
 from app.models import UserVideoWatched
+from django.db.models import Count
 
 
 class UserVideoWatchedAccessor:
 
     @staticmethod
     def get_watched_videos(enrollment_id):
-        return UserVideoWatched.objects.filter(user_course_enrollment=enrollment_id).count()
+        return UserVideoWatched.objects.filter(user_course_enrollment=enrollment_id).aggregate(total_watched=Count('id'))['total_watched']
 
     @staticmethod
     def get_watched_videos_topic(topic_id):
-        return UserVideoWatched.objects.filter(sub_topic__topic=topic_id).count()
+        return UserVideoWatched.objects.filter(sub_topic__topic=topic_id).aggregate(total_watched=Count('id'))['total_watched']
 
     @staticmethod
     def is_video_watched(enrollment_id, sub_topic_id):
