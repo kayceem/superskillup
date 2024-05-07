9aca7f034a23325bb2e3176bc8f91024cb2c18f4
Mohan <mohan@ramailo.tech>
2024-05-07 13:51:18 +0545

    fix:response

--- app/user_video_watched/accessor.py
+++ app/user_video_watched/accessor.py
@@ -8,5 +8,9 @@ class UserVideoWatchedAccessor:
         return UserVideoWatched.objects.filter(user_course_enrollment=enrollment_id).count()
 
     @staticmethod
+    def get_watched_videos_topic(topic_id):
+        return UserVideoWatched.objects.filter(sub_topic__topic=topic_id).count()
+
+    @staticmethod
     def is_video_watched(enrollment_id, sub_topic_id):
         return UserVideoWatched.objects.filter(user_course_enrollment=enrollment_id, sub_topic=sub_topic_id).exists()
